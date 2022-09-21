af55f76beacaea6ba02b0f6d850ecf361464bc97
Danny <dbarahona@ec.krugercorp.com>
2022-09-20 23:04:10 -0500

    translate

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,6 +15,6 @@ return [
 
     'failed' => 'Estas credenciales no coinciden con nuestros registros.',
     'password' => 'La contraseña proporcionada es incorrecta.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor, inténtelo de nuevo en :segundos.',
 
 ];
